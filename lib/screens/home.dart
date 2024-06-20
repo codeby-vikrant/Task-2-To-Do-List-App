ed7feac0bfcbcafa377d94199bac9223f803857d
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-20 10:49:19 +0530

    added menu icon on the app bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_2_to_do_list_app/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("To Do App"),
+        backgroundColor: tdBGColor,
+        title: const Row(
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            )
+          ],
+        ),
       ),
       body: Container(
         child: const Text("This is container"),
