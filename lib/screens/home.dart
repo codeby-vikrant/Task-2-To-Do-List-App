87829cbc7d3776b330417bab808f48d7f49f8efb
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-20 12:15:58 +0530

    made a input box at bottom

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -39,6 +39,35 @@ class HomeScreen extends StatelessWidget {
                   )
                 ],
               )),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  margin:
+                      const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0)
+                      ],
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Add a new todo item',
+                        border: InputBorder.none),
+                  ),
+                ))
+              ],
+            ),
+          )
         ],
       ),
     );
