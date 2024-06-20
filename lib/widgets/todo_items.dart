17ddf3d18f286c1ba691cb3214e3b0f555b1de12
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-20 11:29:22 +0530

    added a delete button

--- lib/widgets/todo_items.dart
+++ lib/widgets/todo_items.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:task_2_to_do_list_app/constants/colors.dart';
 
 class ToDoItem extends StatelessWidget {
   const ToDoItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('ToDo Items');
+    return Container(
+      child: ListTile(
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        tileColor: Colors.white,
+        leading: const Icon(
+          Icons.check_box,
+          color: tdBlue,
+        ),
+        title: const Text(
+          'Check Email',
+          style: TextStyle(
+              fontSize: 16,
+              color: tdBlack,
+              decoration: TextDecoration.lineThrough),
+        ),
+        trailing: Container(
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+              color: tdRed, borderRadius: BorderRadius.circular(5)),
+          child: IconButton(
+              onPressed: () {},
+              iconSize: 16,
+              color: Colors.white,
+              icon: const Icon(
+                Icons.delete,
+              )),
+        ),
+      ),
+    );
   }
 }
