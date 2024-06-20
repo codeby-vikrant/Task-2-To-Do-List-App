0741b7a5dfc4a68b99485d6cda6c2bd9c648f539
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-20 11:09:43 +0530

    made a search box

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,29 +8,57 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: tdBGColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Icon(
-              Icons.menu,
-              color: tdBlack,
-              size: 30,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                child: Image.asset('assets/images/todo.png'),
-              ),
-            )
-          ],
-        ),
-      ),
+      appBar: _buildAppBar(),
       body: Container(
-        child: const Text("This is container"),
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(0),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: tdBlack,
+                        size: 20,
+                      ),
+                      prefixIconConstraints:
+                          BoxConstraints(maxHeight: 20, maxWidth: 25),
+                      border: InputBorder.none,
+                      hintText: 'Search',
+                      hintStyle: TextStyle(color: tdGrey)),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: tdBGColor,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              child: Image.asset('assets/images/todo.png'),
+            ),
+          )
+        ],
       ),
     );
   }
