31840f14c18f09f3d06dcee7e267dccdd30a343b
codeby-vikrant <workwithvikrant0111@gmail.com>
2024-06-20 11:10:49 +0530

    made seperate widget for search box

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,29 +13,32 @@ class HomeScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 15),
           child: Column(
             children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: const TextField(
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(0),
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: tdBlack,
-                        size: 20,
-                      ),
-                      prefixIconConstraints:
-                          BoxConstraints(maxHeight: 20, maxWidth: 25),
-                      border: InputBorder.none,
-                      hintText: 'Search',
-                      hintStyle: TextStyle(color: tdGrey)),
-                ),
-              )
+              searchbox(),
             ],
           )),
+    );
+  }
+
+  Widget searchbox() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, maxWidth: 25),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: tdGrey)),
+      ),
     );
   }
 
